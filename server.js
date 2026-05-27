e4eade0a7744fabb89bfa6d7c971fcf99b85aa44
Garvit Jain <jaingarvit606@gmail.com>
2026-05-27 20:12:58 +0530

    fix render port

--- server.js
+++ server.js
@@ -64,9 +64,11 @@ app.post("/api/data", (req,res)=>{
   });
 });
 
-app.listen(3000, ()=>{
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
 
   console.log(
-    "Server running on http://localhost:3000"
+    "Server running on port " + PORT
   );
 });
